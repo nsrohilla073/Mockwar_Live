8475388e1cb892bc934d992dbc77d092b8feaddb
Nitish Kumar Rohilla <apka-github-wala-email@gmail.com>
2026-05-20 10:21:38 +0530

    Added Pillow and MEDIA_ROOT for KYC photo upload

--- MockWar_backend/settings.py
+++ MockWar_backend/settings.py
@@ -110,6 +110,8 @@ DATABASES = {
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
