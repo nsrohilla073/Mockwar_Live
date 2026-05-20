b24f49bc1997b2415e1b249e158a885449992550
Nitish Kumar Rohilla <apka-github-wala-email@gmail.com>
2026-05-20 20:47:42 +0530

    Database added

--- MockWar_backend/settings.py
+++ MockWar_backend/settings.py
@@ -6,6 +6,8 @@ Generated with Production & Hackproof Security standards.
 import os
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
+
 
 # 1. Build paths inside the project
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,10 +106,10 @@ CHANNEL_LAYERS = {
 # ==========================================
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600
+    )
 }
 
 STATIC_URL = 'static/'
