997820617b48ae8dc41921377d95572c60184e92
Nitish Kumar Rohilla <apka-github-wala-email@gmail.com>
2026-05-21 16:36:29 +0530

    recovered

--- mockwar_frontend/vite.config.js
+++ mockwar_frontend/vite.config.js
@@ -4,17 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/', // Vercel के लिए इसे '/' रखना बेस्ट है
   build: {
     outDir: 'dist',
-    chunkSizeWarningLimit: 1000,
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
-        chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
-        assetFileNames: `assets/[name]-[hash]-${Date.now()}.[ext]`
-      }
-    }
+    chunkSizeWarningLimit: 1000, // फालतू की वॉर्निंग्स बंद करने के लिए
   }
 })
